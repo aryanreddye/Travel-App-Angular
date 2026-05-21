30cb77ee2647dc75baa9f928c81aca1e2ad572b9
aryanreddye <aryanreddyE@gmail.com>
2026-05-21 15:45:40 +0530

    added ngmodel

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
-  standalone:true,
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class Login {
+
+  email: string = '';
+  password: string = '';
+
+  login() {
+    console.log("Login clicked");
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+    if (this.email == "" || this.password == ""){
+      alert("EMpty");
+    }
+    else if (!emailPattern.test(this.email)) {
+    console.log("invalid email formt");
+    }
+    else if (!passwordPattern.test(this.password)){
+      console.log("invalid password")
+    }
+  
+    else{
+    console.log("Email:", this.email);
+    console.log("Password:", this.password);
+  }}
+}

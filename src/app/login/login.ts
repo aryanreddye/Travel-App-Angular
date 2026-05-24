2468f417fb09697365ef9ab71811c23ed180b398
aryanreddye <aryanreddyE@gmail.com>
2026-05-24 12:54:43 +0530

    Integrated Angular Material Design

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatCardModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
